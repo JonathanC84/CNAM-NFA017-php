853a311d4de9c0940d7498a84d114fa7f573ab43
jonathan <jonathan.cayrol@gmail.com>
2021-12-16 12:38:58 +0100

    Création d'un fichier de code strings.php
    - Variables de chaînes de caractères
    - Affichage avec concaténation et interpolation

---
+++ strings.php
@@ -0,0 +1,25 @@
+<?php
+
+$text1 = 'foo';
+
+//concaténation
+
+$text2 = $text1.' bar';
+
+//utilisation des "" pour inclure /n (qui saute une ligne dans le code source html).
+echo $text1."<br>\n";
+echo $text2."<br>\n";
+
+//interpolation (obligatoirement avec "")
+$temperature = 10.5;
+$report = "La température actuelle est de $temperature °C.";
+
+echo $report."<br>\n";
+
+//variante avec {} si l'on doit coller du texte à la variable
+$report = "La température actuelle est de {$temperature}°C.";
+
+echo $report."<br>\n";
+
+//remplacement du point par une virgule (fonction ajoutée à echo)
+echo str_replace('.',',', $report."<br>\n");
